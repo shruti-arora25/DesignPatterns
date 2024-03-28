c1a7f112de44e3032bfa21d158be23a61a164c89
Shruti <shrutiarora2507@gmail.com>
2024-03-28 18:56:15 +0530

    Comit

--- app/src/main/java/com/example/designpatterns/Coroutines/Basic.kt
+++ app/src/main/java/com/example/designpatterns/Coroutines/Basic.kt
@@ -22,81 +22,82 @@ class Basic : AppCompatActivity() {
         setContentView(bind.root)
 
 
-        // doCall()         can't call a suspend method
-        //gives an error as that is coroutine method , should call inside launch method
-//
-//        runBlocking {
-//            Log.d("runblocking", doCall())
-//        }
-//
-//
-//        Log.d("MAinThread--------------->>", Thread.currentThread().name)
-//
-//
-//        GlobalScope.launch(Dispatchers.IO) {
-//            Log.d("FirstG------------->", Thread.currentThread().name)
-//            val call = doCall()
-//            yield()
-//            Log.d("suspend fun", call)
-//
-//
-//
-//            CoroutineScope(Dispatchers.Main).launch {
-//                Log.d("SecondG--------------->>", Thread.currentThread().name)
-//                bind.text.setText("Call")
-//
-//            }
-//
-//
-//            GlobalScope.launch {
-//                Log.d("Scope/------->", "LAunch")
-//            }
-//
-//            withContext(Dispatchers.Main) {
-//                //Name of thread-main
-//                Log.d("THirdContext--------------->>", Thread.currentThread().name)
-//
-//                Log.d("Third--------------->>", call)
-//                bind.text.setText(call)
-//
-//
-//            }
-//
-//
-//
-//            withContext(Dispatchers.Main) {
-//                //Name of thread-main
-//                Log.d("lastContext--------------->>", Thread.currentThread().name)
-//
-//            }
-//
-//
-//            CoroutineScope(Dispatchers.Main).launch {
-//                Log.d("FourthCoroutine-------------->>", Thread.currentThread().name)
-//
-//            }
-//
-//
-//
-//        }
+       //  doCall()         can't call a suspend method
+      //  gives an error as that is coroutine method , should call inside launch method
 
-
-runBlocking {
-    GlobalScope.launch {
-        delay(1000)
-        Log.d("GLobal-------->", "GLobal")
-    }
-}
-
-            Log.d("mainThread--------------->>", Thread.currentThread().name)
+        runBlocking {
+            Log.d("runblocking", doCall())
         }
 
 
+        Log.d("MAinThread--------------->>", Thread.currentThread().name)
+
+
+        GlobalScope.launch(Dispatchers.IO) {
+            Log.d("FirstG------------->", Thread.currentThread().name)
+            val call = doCall()
+            yield()
+            Log.d("suspend fun", call)
+
+
+
+            CoroutineScope(Dispatchers.Main).launch {
+                Log.d("SecondG--------------->>", Thread.currentThread().name)
+                bind.text.setText("Call")
+
+            }
+
+
+            GlobalScope.launch {
+                Log.d("Scope/------->", "LAunch")
+            }
+
+            withContext(Dispatchers.Main) {
+                //Name of thread-main
+                Log.d("THirdContext--------------->>", Thread.currentThread().name)
+
+                Log.d("Third--------------->>", call)
+                bind.text.setText(call)
+            }
+
+            withContext(Dispatchers.Main) {
+                //Name of thread-main
+                Log.d("lastContext--------------->>", Thread.currentThread().name)
+
+            }
+
+
+            CoroutineScope(Dispatchers.Main).launch {
+                Log.d("FourthCoroutine-------------->>", Thread.currentThread().name)
+
+            }
+
+        }
+
+
+//runBlocking {
+//    GlobalScope.launch {
+//        Log.d("GLobal1-------->", "GLobal")
+//    }
+//
+//    CoroutineScope(Dispatchers.Main).launch {
+//        Log.d("Scope-------->", "Coroutine Scope")
+//    }
+//}
+//
+//
+//    GlobalScope.launch {
+//        delay(1000)
+//        Log.d("GLobal2-------->", "GLobal")
+//    }
+
+        Log.d("mainThread--------------->>", Thread.currentThread().name)
+        }
 
 
     private suspend fun doCall(): String {
 
-        //delay(1000)
+        delay(1000)
         return "This is n/w calling"
     }
 }
